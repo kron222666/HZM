3056a1ee5d299e28fffac551948baede358ee1f8
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-04 10:23:49 +0300

    Update ___init___.py

--- src/hza/___init___.py
+++ src/hza/___init___.py
@@ -1,1 +1,10 @@
+"""
+Иерархическая алгебра нулей (HZM)
+Hierarchical Zero Mathematics
+"""
 
+from .core import HierZero
+
+__version__ = "0.1.0"
+__author__ = "Марат Джанбулатов"
+__all__ = ["HierZero"]
