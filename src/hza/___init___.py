4def609f693c0ef8a5a018e2a4367c8cd8c50d10
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-04 10:40:04 +0300

    Update ___init___.py

--- src/hza/___init___.py
+++ src/hza/___init___.py
@@ -1,10 +1,21 @@
 """
-Иерархическая алгебра нулей (HZM)
+HZM - Иерархическая алгебра нулей
 Hierarchical Zero Mathematics
 """
 
 from .core import HierZero
+from .operations import add, sub, mul, div, power, root, neg
+from .projection import projection, HZMProjection
 
 __version__ = "0.1.0"
 __author__ = "Марат Джанбулатов"
-__all__ = ["HierZero"]
+__description__ = "Библиотека для работы с делением на ноль через иерархические уровни"
+
+# Удобные импорты для пользователя
+__all__ = [
+    "HierZero",
+    "add", "sub", "mul", "div", "power", "root", "neg",
+    "projection", "HZMProjection"
+]
+
+# Для удобства: можно писать from hza import HierZero
