312c7bddcaba5ff5f790ff7616b29e327fa08107
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-05 23:08:45 +0300

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,8 @@
+# quick_test.py
+from hzm import HierZero
 
+a = HierZero(10)
+b = HierZero(0, level=2)
+print(a / b)  # должно быть ∞_3
+print(a * b)  # 0_2
+print(HierZero(0, level=1, is_inf=True) ** 2)  # ∞_2
