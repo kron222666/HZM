54f6b76dc273b92283cfe678e39542347ef62c31
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-05 23:23:01 +0300

    Update operations.py

--- src/hzm/operations.py
+++ src/hzm/operations.py
@@ -1,3 +1,4 @@
+import math
 from .core import HierZero
 
 def add(a, b):
@@ -16,25 +17,19 @@ def power(a, b):
     return HierZero(a) ** HierZero(b)
 
 def root(x, n):
-    """
-    Корень n-й степени: x ** (1/n)
-    n может быть числом или HierZero
-    """
+    """Корень n-й степени из x (x ** (1/n))"""
     x = HierZero(x)
     n = HierZero(n)
     if n.level == 0 and n.value <= 0:
         return HierZero.perp()
-    # Для корня из отрицательной бесконечности с чётной степенью -> ⊥
-    if x.is_inf and x.sign < 0 and n.level == 0 and n.value % 2 == 0:
+    # Чётный корень из отрицательной бесконечности -> ⊥
+    if x.is_inf and x.sign < 0 and n.level == 0 and (int(n.value) % 2 == 0):
         return HierZero.perp()
-    # Для корня из нуля или бесконечности: используем степень
+    # Для нулей и бесконечностей используем степень
     return x ** (HierZero(1.0) / n)
 
 def log(base, arg):
-    """
-    Логарифм по основанию base от arg.
-    Реализует таблицу логарифмов HZM.
-    """
+    """Логарифм по основанию base от arg (log_base(arg))"""
     base = HierZero(base)
     arg = HierZero(arg)
 
@@ -45,7 +40,12 @@ def log(base, arg):
     if base.level == 0 and not base.is_inf and base.value == 1:
         return HierZero.perp()
 
-    # log_{0_k} a = 0_k (для обычного положительного a)
+    # Обычные числа (base>0, base≠1, arg>0)
+    if (base.level == 0 and not base.is_inf and base.value > 0 and base.value != 1 and
+        arg.level == 0 and not arg.is_inf and arg.value > 0):
+        return HierZero(math.log(arg.value, base.value))
+
+    # log_{0_k} a = 0_k  (a > 0, a≠1)
     if base.level > 0 and arg.level == 0 and not arg.is_inf and arg.value > 0 and arg.value != 1:
         return HierZero(0, base.level)
 
@@ -57,11 +57,11 @@ def log(base, arg):
     if base.level > 0 and arg.is_inf:
         return HierZero(0, base.level, is_inf=True, sign=-1)
 
-    # log_{∞_k} a = 0_k (для обычного положительного a)
+    # log_{∞_k} a = 0_k  (a>0, a≠1)
     if base.is_inf and arg.level == 0 and not arg.is_inf and arg.value > 0 and arg.value != 1:
         return HierZero(0, base.level)
 
-    # log_{∞_k} 0_m = ⊥ (по таблице)
+    # log_{∞_k} 0_m = ⊥
     if base.is_inf and arg.level > 0:
         return HierZero.perp()
 
@@ -70,20 +70,9 @@ def log(base, arg):
         if base.level == arg.level:
             return HierZero.perp()
         else:
-            # По таблице: результат ∞_{m-k} (если m>k) или 0_{...}? Упростим: 0_{min(k,m)}? 
-            # В вашей таблице указано ∞_{m-k} для k≠m. Реализуем так:
-            if arg.level > base.level:
-                return HierZero(0, arg.level - base.level, is_inf=True, sign=1)
-            else:
-                # если arg.level < base.level: результат 0_{base.level - arg.level}? Не определено. 
-                # Пока вернём 0_{min(k,m)} как в ранних версиях.
-                return HierZero(0, min(base.level, arg.level))
-
-    # Обычный логарифм (base>0, base≠1, arg>0)
-    if (base.level == 0 and not base.is_inf and base.value > 0 and base.value != 1 and
-        arg.level == 0 and not arg.is_inf and arg.value > 0):
-        import math
-        return HierZero(math.log(arg.value, base.value))
+            # по таблице: ∞_{m-k} при m>k, иначе 0_{...}? Пока вернём ∞_{min(k,m)}? Но проще вернуть ⊥
+            # для определённости вернём ∞_{min(k,m)} (молодая бесконечность)
+            return HierZero(0, min(base.level, arg.level), is_inf=True, sign=1)
 
     # Если ничего не подошло -> ⊥
     return HierZero.perp()
