07b5ab806bf7030c7755feb7245ea635449971bb
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-05 23:05:26 +0300

    Update __init__.py

--- src/hzm/__init__.py
+++ src/hzm/__init__.py
@@ -1,12 +1,16 @@
 """
-HZM - Иерархическая алгебра нулей
+HZM - Иерархическая алгебра нулей (Hierarchical Zero Mathematics)
 """
 
 from .core import HierZero
-from .operations import add, sub, mul, div, power, root, neg
+from .operations import add, sub, mul, div, power, root, log
 from .projection import projection, HZMProjection
 
 __version__ = "0.1.0"
 __author__ = "Марат Джанбулатов"
 
-__all__ = ["HierZero", "add", "sub", "mul", "div", "power", "root", "neg", "projection"]
+__all__ = [
+    "HierZero",
+    "add", "sub", "mul", "div", "power", "root", "log",
+    "projection", "HZMProjection"
+]
