f84a643754831e4f6900c78fb9bea54cf942d545
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-06 20:33:57 +0300

    Update __init__.py

--- src/hzm/__init__.py
+++ src/hzm/__init__.py
@@ -1,11 +1,12 @@
-# src/hzm/__init__.py
 from .core import HierZero
 from .projection import project_to_float, from_float
 from .operations import sqrt, log
 
-# Опционально: если PyTorch установлен, подключаем ML-функции
+__all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log"]
+
+# Опциональная интеграция с PyTorch
 try:
     from .ml import HZMAdam, grad_to_hz
-    __all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log", "HZMAdam", "grad_to_hz"]
+    __all__ += ["HZMAdam", "grad_to_hz"]
 except ImportError:
-    __all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log"]
+    pass
