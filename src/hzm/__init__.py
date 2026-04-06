67686507db639c0e590193031174694b13b28558
kron222666 <marat.dzhanbulatov@mail.ru>
2026-04-06 20:21:31 +0300

    Update __init__.py

--- src/hzm/__init__.py
+++ src/hzm/__init__.py
@@ -1,7 +1,11 @@
-"""HZM: Hierarchical Zero Algebra for Python."""
-
+# src/hzm/__init__.py
 from .core import HierZero
 from .projection import project_to_float, from_float
 from .operations import sqrt, log
 
-__all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log"]
+# Опционально: если PyTorch установлен, подключаем ML-функции
+try:
+    from .ml import HZMAdam, grad_to_hz
+    __all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log", "HZMAdam", "grad_to_hz"]
+except ImportError:
+    __all__ = ["HierZero", "project_to_float", "from_float", "sqrt", "log"]
